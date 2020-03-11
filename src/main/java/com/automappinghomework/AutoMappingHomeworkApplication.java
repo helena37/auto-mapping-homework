99562b734ee2bfb88e0cc36bb2a4e50715e4b6dc
Elena <helena.liliya@gmail.com>
2020-03-11 17:32:46 +0200

    ---Initial commit

---
+++ src/main/java/com/automappinghomework/AutoMappingHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.automappinghomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMappingHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMappingHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/automappinghomework/AutoMappingHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automappinghomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMappingHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
